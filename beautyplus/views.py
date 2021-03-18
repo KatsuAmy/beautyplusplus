5860f8ba19ec067f00db9e60b0e50ae3245b40b2
naj <1425497754@qq.com>
2021-03-18 18:49:18 +0800

    1）修改样式不能显示的问题，增加了static文件 2）添加了submit按钮 3）修正了前端选项不能点击的问题

--- beautyplus/views.py
+++ beautyplus/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def main(request):
+    #template = loader.get_template('beautyplus/templates/test.html')
+    context = {}
+    return render(request,'test.html',context)
